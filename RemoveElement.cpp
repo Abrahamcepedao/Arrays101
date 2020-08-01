0f99142671391367496173642c748a832eb3c364
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-01 18:53:26 -0500

    Updated RemoveElement file

--- RemoveElement.cpp
+++ RemoveElement.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int len = 0;
-        for(int i = 0; i < nums.size(); i++){
+        int len = nums.size();
+        int i = 0;
+        while(i < len){
             if(nums[i] == val){
-                if(i < (nums.size()-1)){
-                    for(int j = i+1; j < nums.size(); j++){
-                        nums[j-1] = nums[j]; 
-                    }
-                    nums[nums.size()-1] = val + 1;
-                    i--;
-                }
-                len++;
+                nums[i] = nums[len - 1];
+                len--;
+            } else{
+                i++;
             }
         }
-        len = nums.size() - len;
         return len;
     }
 };
