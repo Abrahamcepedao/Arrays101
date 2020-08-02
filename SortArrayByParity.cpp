a636ac56d4c2ef78b7417ab9bc2334d6ef6350e4
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-02 11:11:24 -0500

    Added SortArrayByParity file

---
+++ SortArrayByParity.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int even = 0;
+        int odd = A.size()-1;
+        int count = 0;
+        int number = A[0];
+        int current = 0;
+        if(A.size() < 2){
+            return A;
+        }
+        while(count < A.size()){
+            if(number % 2 == 0){
+                A[even++] = number;
+                if(even != A.size()){
+                    number = A[even];
+                }
+            } else{
+                current = A[odd];
+                A[odd--] = number;
+                number = current;
+            }
+            count++;
+        }
+        return A;
+    }
+};
